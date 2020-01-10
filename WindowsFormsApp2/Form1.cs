0bfcea6037e1f8ba3003af2fa5c59fc669117104
zls <zls@DESKTOP-F3U79IB>
2020-01-10 17:00:24 +0800

    添加项目文件。

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DevComponents.DotNetBar;
+using process;
+
+namespace test1
+{
+    public partial class Form1 : DevComponents.DotNetBar.Office2007Form
+    {
+        public Form1()
+        {
+
+            this.EnableGlass = false;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("这只是一个测试");
+            // DataProcess dp = new DataProcess();
+            int s = DataProcess.SumData(10, 20);
+
+            MessageBox.Show(s.ToString());
+
+        }
+    }
+}
